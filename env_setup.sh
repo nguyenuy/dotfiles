ce61994f64f1cd982600a250df50bf163dbd4462
Uy Nguyen <nguyenuy@gmail.com>
2016-08-18 22:46:48 -0400

    Add vim setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Install NPM Packages 
-npm install -g tldr
+npm install -g tldr      #tldr man pages
+npm install -g yo        #yeoman generator
+
+# Vim Specific
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
