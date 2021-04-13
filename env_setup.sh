5c370e8062da5b9d2296bd888995a23327680bfa
Uy Nguyen <nguyenuy@gmail.com>
2021-04-13 13:39:59 -0400

    Updates

--- env_setup.sh
+++ env_setup.sh
@@ -13,7 +13,6 @@ chsh -s $(which zsh)
 
 # Install NPM Packages 
 npm install -g tldr      #tldr man pages
-npm install -g yo        #yeoman generator
 
 # Vim Specific
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
